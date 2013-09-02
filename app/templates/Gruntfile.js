ba9d0f527af9d41201a982f976d33dc38670619b
Xavier Julián <xavier.julian@kaleidos.net>
2013-09-02 17:36:02 +0200

    improving main grunt tasks

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -6,6 +6,41 @@ module.exports = function (grunt) {
     require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
     grunt.initConfig({
+        coffee: {
+            dist: {
+                files: [{
+                    expand: true,
+                    src: '{,*/}*.coffee',
+                    dest: 'dist/scripts/',
+                    ext: '.js'
+                }]
+            },
+        },
+        concat: {
+            options: {
+                // define a string to put between each file in the concatenated output
+                separator: ';'
+            },
+            dist: {
+                // the files to concatenate
+                src: ['dist/**/*.js'],
+                // the location of the resulting JS file
+                dest: 'dist/scripts/main.js'
+            }
+        },
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: [
+                        '.tmp',
+                        'dist/*',
+                        'dist/.git*'
+                    ]
+                }]
+            },
+            server: '.tmp'
+        },
         jshint: {
             options: {
                 jshintrc: '.jshintrc'
@@ -26,18 +61,72 @@ module.exports = function (grunt) {
                     src: [
                         '*.{ico,txt,html}',
                         '.htaccess',
-                        'images/{,*/}*.{webp,gif}',
-                        'scripts/{,*/}*.{js,coffee}',
-                        'styles/{,*/}*.{less,css}'
+                        'images/{,*/}*.{webp,gif}'
                     ]
                 }]
             }
+        },
+        uglify: {
+            options: {
+                // the banner is inserted at the top of the output
+                banner: '/*! <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/main.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 9001,
+                    base: 'dist'
+                }
+            }
+        },
+        watch: {
+            coffee: {
+                files: ['app/scripts/{,*/}*.coffee'],
+                tasks: ['coffee:dist']
+            },
         }
+        //watch: {
+            //coffee: {
+                //files: ['app/scripts/{,*/}*.coffee'],
+                //tasks: ['coffee:dist']
+            //},
+            //recess: {
+                //files: ['<%= yeoman.app %>/styles/{,*/}*.less'],
+                //tasks: ['recess:dist']
+            //},
+            //livereload: {
+                //options: {
+                    //livereload: LIVERELOAD_PORT
+                //},
+                //files: [
+                    //'dist/*.html',
+                    //'{css,dist/styles/{,*/}*.css, dist/styles/{,*/}*.css',
+                    //'{js,dist/scripts/{,*/}*.js',
+                    //'dist/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+                //]
+            //}
+        //},
     });
 
     grunt.registerTask('default', [
         'jshint',
-        'copy:dist'
+        'clean:dist',
+        'copy:dist',
+        'coffee:dist',
+        'connect:server',
+        'minify'
+    ]);
+    
+    grunt.registerTask('minify', [
+        'concat:dist',
+        'uglify:dist',
+        'watch',
     ]);
 
 };
